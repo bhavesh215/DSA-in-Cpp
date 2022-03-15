be4d31c6a747b6ee38283992bdaba1320d253c98
bhavesh215 <bhaveshkumargci2018@gmail.com>
2022-03-15 15:46:13 +0530

    Learnt Basics of a program and variables and datatypes in c++ and also learnt input and output statement in c++

---
+++ input_and_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amount_by_mother;
+    cin>>amount_by_mother;
+    int amount_by_father;
+    cin>>amount_by_father;
+    int sum=amount_by_father+amount_by_mother;
+    cout<<"Total amount = "<<sum;
+    return 0;
+}
---
+++ variables_and_Datatype.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    char c;
+    bool d;
+    cout<<"Size of int "<<sizeof(a)<<endl;
+    cout<<"Size of float "<<sizeof(b)<<endl;
+    cout<<"Size of char "<<sizeof(c)<<endl;
+    cout<<"Size of bool "<<sizeof(d)<<endl;
+
+    short int si;
+    long int li;
+    cout<<"Size of short int "<<sizeof(si)<<endl;
+    cout<<"Size of long int "<<sizeof(li)<<endl;
+}
